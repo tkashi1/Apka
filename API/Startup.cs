e31889d897c9432bbf5765a001e7ef0210cc6035
tkashi1 <takashiofficial89@gmail.com>
2022-04-01 10:19:05 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,8 +55,11 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
+            
+            app.UseAuthentication();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
